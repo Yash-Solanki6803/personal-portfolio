5092e1bb07c014cc8b0ddc8aad2b8c179e096c17
Yash Solanki <thisisyashs@gmail.com>
2024-06-23 11:21:21 +0530

    Frontend : Admin Navlink Active State

--- src/ui/components/Admin/NavLink/Navlink.jsx
+++ src/ui/components/Admin/NavLink/Navlink.jsx
@@ -1,10 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 function Navlink({ href = "/", children, className = "" }) {
+  const pathname = usePathname();
   return (
     <Link
       href={href}
       className={
-        "bg-[#696969] px-4 py-2 rounded-full text-xs w-full font-RubikMedium text-neutral-50 hover:bg-[#4a4a4a]  transition-all ease-in duration-300" +
+        " px-4 py-2 rounded-full text-xs w-full font-RubikMedium text-neutral-50 hover:bg-[#4a4a4a]  transition-all ease-in duration-300" +
+        (pathname === href ? " bg-[#4a4a4a] " : "bg-transparent ") +
         className
       }
     >
