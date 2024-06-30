caa15eeb29adb93fba05f3f219823cb8e34ab9ab
Yash Solanki <thisisyashs@gmail.com>
2024-06-30 11:20:35 +0530

    Loader change

--- src/ui/components/Loader/Loader.jsx
+++ src/ui/components/Loader/Loader.jsx
@@ -1,8 +1,10 @@
 function Loader({ size = 10 }) {
   return (
-    <div
-      className={`w-${size} h-${size} border-2 border-transparent border-t-white  rounded-full animate-spin`}
-    ></div>
+    <div class="relative inline-flex">
+      <div class="w-8 h-8 bg-slate-400 rounded-full"></div>
+      <div class="w-8 h-8 bg-slate-300 rounded-full absolute top-0 left-0 animate-ping"></div>
+      <div class="w-8 h-8 bg-slate-200 rounded-full absolute top-0 left-0 animate-pulse"></div>
+    </div>
   );
 }
 
